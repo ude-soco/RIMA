d378a1d090d858e17e23933d70d8db38b2bc5615
Ralf Berger <ralf.berger@uni-due.de>
2021-02-22 16:50:50 +0100

    Fetch model only if not already present

--- RIMA-Backend/bin/worker.sh
+++ RIMA-Backend/bin/worker.sh
@@ -7,8 +7,12 @@ set -o nounset
 echo "Migrating database ..." &&\
   python manage.py migrate &&\
 
-  echo "Fetching model file ..." &&\
+if [ -f "GLOVE_MODEL_FILE_PATH" ]; then
+  echo "GloVe model present: $GLOVE_MODEL_FILE_PATH"
+else
+  echo "GloVe model missing; downloading now ..." &&\
     $HOME/.local/bin/gdown --id $GLOVE_MODEL_GDRIVE_ID -O $GLOVE_MODEL_FILE_PATH &&\
+fi &&\
 
   echo "Starting Celery job queue ..." &&\
   celery --pidfile=/opt/celeryd.pid worker --app=interest_miner_api -l info
