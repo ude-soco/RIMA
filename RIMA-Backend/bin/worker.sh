92076e48e87f884b248417cef1d2ea7755a14f03
Ralf Berger <ralf.berger@uni-due.de>
2021-02-22 16:32:20 +0100

    Fetch model on startup, resolve #9
    * add gdown via Pip
    * set up model cache volume in Docker Compose

--- RIMA-Backend/bin/worker.sh
+++ RIMA-Backend/bin/worker.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -o nounset
 
+: $GLOVE_MODEL_GDRIVE_ID
+: $GLOVE_MODEL_FILE_PATH
+
 echo "Migrating database ..." &&\
   python manage.py migrate &&\
+
+  echo "Fetching model file ..." &&\
+    $HOME/.local/bin/gdown --id $GLOVE_MODEL_GDRIVE_ID -O $GLOVE_MODEL_FILE_PATH &&\
 
   echo "Starting Celery job queue ..." &&\
   celery --pidfile=/opt/celeryd.pid worker --app=interest_miner_api -l info
