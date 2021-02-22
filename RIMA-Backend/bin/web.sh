38466046c3d71e9ef64e612447819475e1e46652
Ralf Berger <ralf.berger@uni-due.de>
2021-02-22 16:01:46 +0100

    Log requests to stdout

--- RIMA-Backend/bin/web.sh
+++ RIMA-Backend/bin/web.sh
@@ -4,4 +4,5 @@ echo "Starting Nginx ..." &&\
   nginx -c $HOME/nginx.conf -g "pid $HOME/nginx.pid;" &&\
 
 echo "Starting Gunicorn ..." &&\
-  gunicorn --timeout 600 interest_miner_api.wsgi:application -b 0.0.0.0:8000 --log-level info
+  gunicorn --timeout 600 interest_miner_api.wsgi:application -b 0.0.0.0:8000 \
+    --log-level info --access-logfile -
