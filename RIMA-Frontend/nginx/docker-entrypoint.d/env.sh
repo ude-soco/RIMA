446c816db86e3220358ab212ca168677b0cdf0fe
Ralf Berger <ralf.berger@uni-due.de>
2021-03-12 10:34:43 +0100

    Configure RIMA-Frontend BASE_URL on container startup

--- RIMA-Frontend/nginx/docker-entrypoint.d/env.sh
+++ RIMA-Frontend/nginx/docker-entrypoint.d/env.sh
@@ -3,5 +3,10 @@ set -euo pipefail
 
 echo "Configuring application based on ENV ..."
 
-# :TODO: Write $API_BASE_URL (could default to http://127.0.0.1:8000/api/)
-#        to a config file that is processed by the React app during runtime
+ESCAPED_DUMMY_API_BASE_URL=$(echo "http://127.0.0.1:8000/api" | sed -e 's/\([[\/.*]\|\]\)/\\&/g')
+ESCAPED_REAL_API_BASE_URL=$(printf '%s\n' "$API_BASE_URL" | sed -e 's/[\/&]/\\&/g')
+
+find /usr/share/nginx/html -type f -print0 | \
+  xargs -0 sed -i  "s/$ESCAPED_DUMMY_API_BASE_URL/$ESCAPED_REAL_API_BASE_URL/g"
+
+# :TODO: Write $API_BASE_URL to a config file that is processed by the React app during runtime
