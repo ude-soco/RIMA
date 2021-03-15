3f8d15e25f0a1656fb3bbe9f1e1239587ac65f49
Ralf Berger <ralf.berger@uni-due.de>
2021-03-15 10:37:23 +0100

    Configure RIMA-Frontend BASE_URL on container startup (2)

--- RIMA-Frontend/nginx/docker-entrypoint.d/env.sh
+++ RIMA-Frontend/nginx/docker-entrypoint.d/env.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Configuring application based on ENV ..."
+DUMMY_BASE_URL="http://127.0.0.1:8000"
+ESCAPED_DUMMY_BASE_URL=$(echo "$DUMMY_BASE_URL" | sed -e 's/\([[\/.*]\|\]\)/\\&/g')
 
-ESCAPED_DUMMY_API_BASE_URL=$(echo "http://127.0.0.1:8000/api" | sed -e 's/\([[\/.*]\|\]\)/\\&/g')
-ESCAPED_REAL_API_BASE_URL=$(printf '%s\n' "$API_BASE_URL" | sed -e 's/[\/&]/\\&/g')
+ESCAPED_BASE_URL=$(printf '%s\n' "$BASE_URL" | sed -e 's/[\/&]/\\&/g')
 
-find /usr/share/nginx/html -type f -print0 | \
-  xargs -0 sed -i  "s/$ESCAPED_DUMMY_API_BASE_URL/$ESCAPED_REAL_API_BASE_URL/g"
-
-# :TODO: Write $API_BASE_URL to a config file that is processed by the React app during runtime
+echo "Rewriting $DUMMY_BASE_URL to $BASE_URL ..."
+find /usr/share/nginx/html -type f -exec \
+  sed -i "s#$ESCAPED_DUMMY_BASE_URL#$ESCAPED_BASE_URL#g" {} \;
