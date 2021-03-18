51733838917f96540cc1693175e36f0d86071b5c
Ralf Berger <ralf.berger@uni-due.de>
2021-03-18 12:24:45 +0100

    Download model in init container

---
+++ RIMA-Backend/bin/download.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+: $GLOVE_MODEL_GDRIVE_ID
+: $GLOVE_MODEL_FILE_PATH
+
+if [ -f "$GLOVE_MODEL_FILE_PATH" ]; then
+  echo "GloVe model present: $GLOVE_MODEL_FILE_PATH"
+else
+  echo "GloVe model missing; downloading now ..."
+  $HOME/.local/bin/gdown --id $GLOVE_MODEL_GDRIVE_ID -O $GLOVE_MODEL_FILE_PATH
+fi
