789fc394dbdff9fa9b64a963650b1e93a53c2efd
Ralf Berger <ralf.berger@uni-due.de>
2021-04-01 10:44:27 +0200

    Update RIMA-Backend STATIC_ROOT
    Work around frontend and backend using the same path for their assets.

--- RIMA-Backend/interest_miner_api/settings.py
+++ RIMA-Backend/interest_miner_api/settings.py
@@ -165,8 +165,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # No of days for which the tweets needs to be imported
 TWITTER_FETCH_DAYS = int(os.getenv(
