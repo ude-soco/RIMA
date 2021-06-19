6402727a4c0bf0ae75ce81015d33981c88e5c570
Basem Abughallya <basem.maher45@gmail.com>
2021-06-19 16:35:08 +0200

    adding conferences app to admin

--- RIMA-Backend/conferences/admin.py
+++ RIMA-Backend/conferences/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import * 
 
-# Register your models here.
+admin.site.register(Platform)
+admin.site.register(Conference)
+admin.site.register(Conference_Event)
+admin.site.register(Conference_Event_Paper)
+admin.site.register(Author)
