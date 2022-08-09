55dc38048bb40517e384a803bc546a717bf12fb6
Shoeb Joarder <shoebjoarder@outlook.com>
2022-08-10 01:52:51 +0200

    Update HowExplanation.jsx

--- RIMA-Frontend/nodejs/src/Views/Application/Publications/Recommendation/Components/HowExplanation.jsx
+++ RIMA-Frontend/nodejs/src/Views/Application/Publications/Recommendation/Components/HowExplanation.jsx
@@ -1,24 +1,15 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import EmbeddingLegends from "./EmbeddingLegends";
-
-const textMetrics = require("text-metrics");
-const el = document.querySelector("h5");
-const metrics = textMetrics.init(el);
-
-import {   
-    Typography,
-    Tabs,
-    Tab,
-    Box,
-
-     } from "@material-ui/core";
-import PropTypes from 'prop-types';
+import { Typography, Tabs, Tab, Box } from "@material-ui/core";
+import PropTypes from "prop-types";
 import HowStep from "./HowStep";
 import HowFinalStep from "./HowFinalStep";
 import HowOverview from "./HowOverview";
 
-
+const textMetrics = require("text-metrics");
+const el = document.querySelector("h5");
+const metrics = textMetrics.init(el);
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -49,7 +40,7 @@ TabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `vertical-tab-${index}`,
-    'aria-controls': `vertical-tabpanel-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`,
   };
 }
 
@@ -57,57 +48,50 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
-    display: 'flex',
-    height: '100%',
+    display: "flex",
+    height: "100%",
   },
   tabs: {
     borderRight: `1px solid ${theme.palette.divider}`,
     display: "flex !important",
     justifyContent: "space-evenly !important",
-  },  
+  },
   TabPanel: {
     width: "100%",
-
   },
   collapseButton: {
     width: "100px",
     display: "flex",
-      justifyContent: "flex-end",
-    },
-    checkboxLabel: {
-      fontSize:"16pt"
-      },
-
+    justifyContent: "flex-end",
+  },
+  checkboxLabel: {
+    fontSize: "16pt",
+  },
 }));
-  
 
 export default function HowExplanation(props) {
+  // start Tannaz
+  const classes = useStyles();
 
+  // states
+  const [state, setState] = useState({
+    paper: props.paper,
+    interests: props.interests,
+    index: props.index,
+  });
 
-    // start Tannaz
-    const classes = useStyles();
+  // console.log(state.paper);
+  // console.log(state.interests);
+  // console.log(state.index);
 
-    // states
-      const [state, setState] = useState({
-        paper: props.paper,
-        interests: props.interests,
-        index: props.index,
-    })
-  
-    // console.log(state.paper);
-    // console.log(state.interests);
-    // console.log(state.index);
-  
+  const [value, setValue] = React.useState(0);
 
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-    const [value, setValue] = React.useState(0);
-  
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    };
-
-    return (
-      <>
+  return (
+    <>
       <Tabs
         orientation="vertical"
         indicatorColor="primary"
@@ -121,34 +105,37 @@ export default function HowExplanation(props) {
         centered
       >
         <Tab label="OVERVIEW" {...a11yProps(0)} />
-        <Tab label="GET USER INTERESTS AND PUBLICATION KEYWORDS / KEYPHRASES" {...a11yProps(1)} />
+        <Tab
+          label="GET USER INTERESTS AND PUBLICATION KEYWORDS / KEYPHRASES"
+          {...a11yProps(1)}
+        />
         <Tab label="GENERATE EMBEDDINGS" {...a11yProps(2)} />
         <Tab label="COMPUTE SIMILARITY" {...a11yProps(3)} />
-        </Tabs>
-        <TabPanel style={{width:"70%"}} value={value} index={0}>
-            <HowOverview paper={state.paper}  />
-            <EmbeddingLegends />
-        </TabPanel>
-        {/*-----hoda-start---*/}
-        <TabPanel style={{width:"70%"}} value={value===1 || value===2?1:value} index={1}>
-          <HowStep
-              interests={state.interests}
-              keywords={state.paper.keyword}
-              inclEmbedding={value===2}
-            />
-          <EmbeddingLegends />
-        </TabPanel>
-        <TabPanel  style={{width:"70%"}} value={value} index={3}>
-          <HowFinalStep paper={state.paper} />
-          <EmbeddingLegends />
-        </TabPanel>
-        {/*-----hoda-end---*/}
-      </>
-    );
+      </Tabs>
+      <TabPanel style={{ width: "70%" }} value={value} index={0}>
+        <HowOverview paper={state.paper} />
+        <EmbeddingLegends />
+      </TabPanel>
+      {/*-----hoda-start---*/}
+      <TabPanel
+        style={{ width: "70%" }}
+        value={value === 1 || value === 2 ? 1 : value}
+        index={1}
+      >
+        <HowStep
+          interests={state.interests}
+          keywords={state.paper.keyword}
+          inclEmbedding={value === 2}
+        />
+        <EmbeddingLegends />
+      </TabPanel>
+      <TabPanel style={{ width: "70%" }} value={value} index={3}>
+        <HowFinalStep paper={state.paper} />
+        <EmbeddingLegends />
+      </TabPanel>
+      {/*-----hoda-end---*/}
+    </>
+  );
 
-            // end Tannaz
-
-    }
-
-
-
+  // end Tannaz
+}
