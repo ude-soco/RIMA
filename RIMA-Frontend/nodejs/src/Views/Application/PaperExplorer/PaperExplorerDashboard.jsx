b880694758d414e7dcb908ec0e0ebd2d44867b36
Shoeb Joarder <shoebjoarder@outlook.com>
2023-03-02 12:57:36 +0100

    added paper explorer dashboard

---
+++ RIMA-Frontend/nodejs/src/Views/Application/PaperExplorer/PaperExplorerDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Typography} from "@material-ui/core";
+
+const PaperExplorerDashboard = () => {
+  return (
+    <>
+      <Typography>Paper Explorer works</Typography>
+    </>
+  );
+}
+
+export default PaperExplorerDashboard;
