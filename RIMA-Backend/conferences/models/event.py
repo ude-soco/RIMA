672aa573802f1bb84301022fcb970674d608fc81
IslamAbdelghaffar <98360734+IslamAbdelghaffar@users.noreply.github.com>
2023-04-06 15:00:54 +0200

    RIMA-Backend: event class: has_auther relationship added

--- RIMA-Backend/conferences/models/event.py
+++ RIMA-Backend/conferences/models/event.py
@@ -1,7 +1,9 @@
 from neomodel import (config, StructuredNode, StringProperty, IntegerProperty,
                       UniqueIdProperty, RelationshipTo)
-
+from .author import Author
 
 class Event(StructuredNode):
     conference_event_name_abbr = StringProperty()
     conference_event_url = StringProperty()
+
+    authors = RelationshipTo(Author, 'has_author')
