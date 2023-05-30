8e6e914a21a6eb05b9bfba0a7b4acef296f314df
PhilippUDE <129851431+PhilippUDE@users.noreply.github.com>
2023-05-30 18:51:11 +0200

    Update Connect.jsx

--- RIMA-Frontend/nodejs/src/Views/Application/InterestProfile/Tabs/Connect/Connect.jsx
+++ RIMA-Frontend/nodejs/src/Views/Application/InterestProfile/Tabs/Connect/Connect.jsx
@@ -1,21 +1,44 @@
 import * as React from "react";
 import ConnectedGraph from "./ConnectedGraph"
-import {CircularProgress, Grid, Typography} from "@material-ui/core";
+import {CircularProgress, Grid, Typography, Box, TextField, Button} from "@material-ui/core";
 import {useEffect, useState} from "react";
 import RestAPI from "../../../../../Services/api";
 import Help from "./Help"
+import FilterListIcon from "@material-ui/icons/FilterList";
 
 export default function Connect (props) {
-    const {data} = props
-    console.log(data, "data connect")
-
+    //let {data} = props
+    const [data, setData] = useState(props.data)
+    console.log(data, "const")
+    console.log(data, "useState")
     const [dataCollected, setDataCollected]=useState(false)
     const [myInterests, setMyInterests]=useState([])
+    const [noa, setNoa] = useState(3)
     let currentUser = JSON.parse(localStorage.getItem("rimaUser"));
+    
+    
 
 
+    //ChatGPT
+    const submitNumber = async() => {
+        const inputValue = document.getElementById("noa").value;
+        const numberOfAuthors = parseInt(inputValue, 10);
+      
+        setNoa(numberOfAuthors);
+        setDataCollected(false);
+      
+        RestAPI.getConnectData({ data: currentUser.author_id, noa: noa })
+          .then((res) => {
+            const {data}=res
+            setData(data.data)
+            console.log(data, "useState2")
+          })
+          .catch((error) => {
+            console.log("Error fetching data:", error);
+          });
+      };
+ 
     useEffect(()=>{
-
         if(data.length===0){
             setDataCollected(false)
         }
@@ -28,26 +51,50 @@ export default function Connect (props) {
                 })
                 setMyInterests(interests)
             })
+                     
+
             setDataCollected(true)
         }
 
 
     },[data])
 
-    console.log(data, "data connect")
+
+
+    
+
+
+    console.log(data, "const3")
     return (
         <>
             <Help/>
             <Grid container>
-
-                <Grid item xs ={12} style={{padding:"8px"}}>
+              <Grid item xs ={12} style={{padding:"8px"}}>
+                    <Box display="flex" justifyContent="flex-end" alignItems="flex-end">
+                        <TextField  id="noa"
+                        label="Number"
+                        type="number"
+                        variant="outlined"
+                        size="small"
+                        defaultValue = "3"
+                        color="primary"
+                        style={{ width: "7.5%" }}      
+                        inputProps={{
+                            min: 0,
+                            max: 10,
+                            step: 1,
+                        }}  
+                        />
+                        <Button onClick={submitNumber} id="submit" color="primary">Submit</Button>
+                        <Button startIcon={<FilterListIcon/>} color="primary" >
+                            MORE
+                        </Button>
+                    </Box>
                     {dataCollected?
                         <ConnectedGraph data={data} myInterests={myInterests}/>:<Loading/>
                     }
-
-
+             
                 </Grid>
-
             </Grid>
         </>
         )
@@ -72,3 +119,5 @@ export const Loading = () => {
         </>
     )
 }
+
+
