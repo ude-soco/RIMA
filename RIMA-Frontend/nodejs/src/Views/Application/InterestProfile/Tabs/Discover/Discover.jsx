a5ed649bd928e91fb8dc4f07bd5c2bbafe03b1de
Mervenur Seyhan <s-merve-nur@hotmail.de>
2023-06-26 10:56:32 +0200

    information button

--- RIMA-Frontend/nodejs/src/Views/Application/InterestProfile/Tabs/Discover/Discover.jsx
+++ RIMA-Frontend/nodejs/src/Views/Application/InterestProfile/Tabs/Discover/Discover.jsx
@@ -365,7 +365,8 @@ const Popup = ({ onClose }) => {
   return (
     <div>
       <h3>Information</h3>
-      <p>On this page, you can explore interests that go beyond your usual topics. These controversial interests aim to introduce you to new subject areas and expand your knowledge.</p>
+      <p>On this page, you can explore interests that go beyond your usual topics. These controversial interests aim to introduce you to new subject areas and expand your knowledge.
+        Moreover you can add your interest to your favorite-interests table. </p>
       <button onClick={onClose}>Close</button>
     </div>
   );
