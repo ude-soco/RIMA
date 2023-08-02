19f70ab15f44987936d8c6a055bbb4c4066c80a9
Ralf Berger <ralf.berger@uni-due.de>
2023-08-02 10:49:31 +0200

    Add derived path constant BASE_URL_CONFERENCE
    + Remove configuration option for derived paths

--- RIMA-Frontend/nodejs/src/Services/constants/index.js
+++ RIMA-Frontend/nodejs/src/Services/constants/index.js
@@ -1,4 +1,5 @@
 export const BASE_URL = process.env.REACT_APP_BASE_URL || "http://127.0.0.1:8000";
-export const BASE_URL_INTEREST = process.env.REACT_APP_BASE_URL_INTEREST || BASE_URL + "/api/interests/";
+export const TOKEN    = process.env.REACT_APP_TOKEN    || "cdf895267756d954747b63b9cc56e23fa06ecd29";
 
-export const TOKEN = process.env.REACT_APP_TOKEN || "cdf895267756d954747b63b9cc56e23fa06ecd29";
+export const BASE_URL_INTEREST   = BASE_URL + "/api/interests/";
+export const BASE_URL_CONFERENCE = BASE_URL + "/api/conferences/";
