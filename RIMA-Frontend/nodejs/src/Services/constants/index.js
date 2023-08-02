c6102719b448689eda61733b60e62766498fbe88
Ralf Berger <ralf.berger@uni-due.de>
2023-08-02 10:30:50 +0200

    Set defaults for BASE_URL and others

--- RIMA-Frontend/nodejs/src/Services/constants/index.js
+++ RIMA-Frontend/nodejs/src/Services/constants/index.js
@@ -1,5 +1,4 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const BASE_URL_INTEREST = process.env.REACT_APP_BASE_URL_INTEREST;
-export const BASE_URL_CONFERENCE = process.env.REACT_APP_BASE_URL_CONFERENCE;
+export const BASE_URL = process.env.REACT_APP_BASE_URL || "http://127.0.0.1:8000";
+export const BASE_URL_INTEREST = process.env.REACT_APP_BASE_URL_INTEREST || BASE_URL + "/api/interests/";
 
-export const TOKEN = process.env.REACT_APP_TOKEN;
+export const TOKEN = process.env.REACT_APP_TOKEN || "cdf895267756d954747b63b9cc56e23fa06ecd29";
